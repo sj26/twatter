c2ddf878d73a3811b1e365897b58f10cc7cf1976
Samuel Cochran <sj26@sj26.com>
2012-01-15 00:35:40 +1030

    Shoulda been 1.9

--- db/migrate/20120114125457_devise_create_users.rb
+++ db/migrate/20120114125457_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :username,             :unique => true
-    add_index :users, :email,                :unique => true
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
