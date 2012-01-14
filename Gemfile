177ff4479ffeff980803e1ea53328e3dbe9fbbc9
Samuel Cochran <sj26@sj26.com>
2012-01-14 23:22:31 +1030

    New rails project

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass'
+end
+
+gem 'jquery-rails'
+gem 'devise'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twatter::Application.config.secret_token = '0a61b663d165cf724be9228e9c1a49926f22f639a9dbe9d0aed55bc47f3ca1be15d9055712abe802350a8348b7904626fda0ab1f075de9acca42a00103ee193c'
