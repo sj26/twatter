98508bb7b23daa2eac6893c0dc6d36a5e0ee9606
Samuel Cochran <sj26@sj26.com>
2012-01-14 23:41:04 +1030

    Add user model via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Twatter::Application.routes.draw do
+  devise_for :users
+
   root to: "home#show"
 end
---
+++ db/migrate/20120114125457_devise_create_users.rb
@@ -0,0 +1,14 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      t.string :username, null: false
+      t.database_authenticatable null: false
+      t.rememberable
+      t.trackable
+      t.timestamps
+    end
+
+    add_index :users, :username,             :unique => true
+    add_index :users, :email,                :unique => true
+  end
+end
