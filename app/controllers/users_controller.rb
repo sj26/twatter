b122a0b50617b63e32504afb42d7d64545afcab0
Samuel Cochran <sj26@sj26.com>
2012-01-15 17:28:53 +1030

    The helper way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,35 @@
 class UsersController < ApplicationController
+  before_filter :prepare_user
+  before_filter :prepare_user
+
   def show
-    @user = User.find_by_username! params[:id]
   end
 
   def edit
-    @user = User.find_by_username! params[:id]
   end
 
   def update
-    @user = User.find_by_username! params[:id]
-    if @user.update_attributes params[:user]
-      redirect_to @user, notice: "Profile updated"
+    if user.update_attributes params[:user]
+      redirect_to user, notice: "Profile updated"
     else
       render :edit, alert: "Not quite"
     end
   end
 
   def follow
-    @user = User.find_by_username! params[:id]
-    current_user.follower_follows.create! followee: @user
-    redirect_to :back, notice: "Following #{@user}"
+    current_user.follower_follows.create! followee: user
+    redirect_to :back, notice: "Following #{user}"
   end
 
   def unfollow
-    @user = User.find_by_username! params[:id]
-    current_user.follower_follows.where(followee_id: @user.id).destroy_all
-    redirect_to :back, notice: "No longer following #{@user}"
+    current_user.follower_follows.where(followee_id: user.id).destroy_all
+    redirect_to :back, notice: "No longer following #{user}"
   end
+
+protected
+
+  def user
+    @user ||= User.find_by_username! params[:id]
+  end
+  helper_method :user
 end
