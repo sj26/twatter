44a15f166ec9c598c2c880c58d47d1b28236e80f
Samuel Cochran <sj26@sj26.com>
2012-01-15 19:28:34 +1030

    Mentions formatting

---
+++ app/helpers/tweets_helper.rb
@@ -0,0 +1,11 @@
+module TweetsHelper
+  def format_tweet tweet
+    h(tweet.text).gsub(/@([a-z0-9._-]{3,15})/i) do |mention|
+      if user = User.find_by_username(mention.from(1))
+        content_tag(:span, "@#{link_to user, user}".html_safe, class: "mention").html_safe
+      else
+        username
+      end
+    end.html_safe
+  end
+end
